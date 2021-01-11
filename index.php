f4a8916906d633f8a21bfa38740f49b1f3481943
Andrew Hawkes <adw.hwk@gmail.com>
2021-01-11 16:04:11 +1100

    Days progress

--- index.php
+++ index.php
@@ -1,25 +1,358 @@
 <?php get_header(); ?>
 
 
-<div class="hero-wrapper">
+<div id="page-wrapper">
 
-    <div class="swiper-container hero-swiper-container">
-        <div class="hero-article-wrapper swiper-wrapper">
+    <header>
+
+        <a id="prev-month" class="month-navigation" style="opacity: 0;"><i class="fas fa-angle-double-left"></i></a>
+
+        <a class="month-text"><strong>2021</strong> | Jan <i class="fas fa-angle-down"></i></a>
+
+        <a id="next-month" class="month-navigation"><i class="fas fa-angle-double-right"></i></a>
+
+        <div class="months-dropdown-menu">
+        
+            <div class="month">
+                <div class="banner">01</div>
+                <div class="body">January</div>
+            </div>
+            <div class="month">
+                <div class="banner">02</div>
+                <div class="body">February</div>
+            </div>
+            <div class="month">
+                <div class="banner">03</div>
+                <div class="body">March</div>
+            </div>
+            <div class="month">
+                <div class="banner">04</div>
+                <div class="body">April</div>
+            </div>
+            <div class="month">
+                <div class="banner">05</div>
+                <div class="body">May</div>
+            </div>
+            <div class="month">
+                <div class="banner">06</div>
+                <div class="body">June</div>
+            </div>
+            <div class="month">
+                <div class="banner">07</div>
+                <div class="body">July</div>
+            </div>
+            <div class="month">
+                <div class="banner">08</div>
+                <div class="body">August</div>
+            </div>
+            <div class="month">
+                <div class="banner">09</div>
+                <div class="body">September</div>
+            </div>
+            <div class="month">
+                <div class="banner">10</div>
+                <div class="body">October</div>
+            </div>
+            <div class="month">
+                <div class="banner">11</div>
+                <div class="body">November</div>
+            </div>
+            <div class="month">
+                <div class="banner">12</div>
+                <div class="body">December</div>
+            </div>
+
+
+        </div>
+
+
+    </header>
+
+
+    <div class="calendar-swiper swiper-container">
+        <div class="swiper-wrapper">
+
+
+            <div class="calendar-slide swiper-slide" id="january-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+                        <div class="upper"></div>
+
+                        <div class="lower"></div>
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="february-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="march-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="april-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="may-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="june-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="july-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="august-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="september-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="october-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="november-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+            <div class="calendar-slide swiper-slide" id="december-slider">
+
+                <div class="hero-wrapper">
+
+                    <div class="swiper-container hero-swiper-container">
+                        <div class="hero-article-wrapper swiper-wrapper">
+
+
+
+                        </div>
+                    </div>
+                    <div class="hero-text-wrapper">
+
+
+
+                    </div>
+                </div>
+                <div class="event-wrapper">
+
+
+
+                </div>
+            </div>
+
 
         </div>
     </div>
-
-    <div class="hero-text-wrapper">
-
-    </div>
-
-</div>
-
-
-
-<div class="event-wrapper">
-
-   
 
 </div>
 
